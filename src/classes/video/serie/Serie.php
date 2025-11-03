a2e38603bc7617e2eab26a25019b94f5d7626fe5
BOUDOUAH-Ilias <ilias.boudouah8@etu.univ-lorraine.fr>
2025-11-03 17:17:37 +0100

    [Feature] Ajout des attributs de Serie

---
+++ src/classes/video/serie/Serie.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace iutnc\netvod\video\serie;
+
+class Serie {
+    private String $genre;
+    private String $typePublic;
+    private Array $liste;
+    private String $titre;
+    private String $descriptif; 
+    private int $annee;
+    private String $dateAjout;
+    private int $nbEpisode;
+    private String $cheminImage;
+
+}
