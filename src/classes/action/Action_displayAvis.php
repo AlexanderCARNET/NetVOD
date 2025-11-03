4a08ed99daee1dcfdf9c33ee275fb1c4c23755e2
Alexander CARNET <e67005u@univ-lorraine.fr>
2025-11-03 17:16:24 +0100

    [INIT] creation des class Action_noter.php et Action_displayAvis.php

---
+++ src/classes/action/Action_displayAvis.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+class Action_displayAvis extends Action
+{
+
+    public function execute(): string
+    {
+        return "";
+    }
+}
---
+++ src/classes/action/Action_noter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+class Action_noter extends Action
+{
+
+    public function execute(): string
+    {
+        return "";
+    }
+}
