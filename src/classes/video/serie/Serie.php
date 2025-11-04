19abf503174016d48ea91a37b8311d672802f750
Florian HANNEZO <florian.hannezo5@etu.univ-lorraine.fr>
2025-11-04 17:30:31 +0100

    [MODIFY] Ajout de l'attribut id et de la méthode addEpisode. Constructeur modifié pour id

--- src/classes/video/serie/Serie.php
+++ src/classes/video/serie/Serie.php
@@ -2,27 +2,34 @@
 
 namespace iutnc\netvod\video\serie;
 
+use DateTime;
+use DateTimeZone;
+use iutnc\netvod\video\episode\Episode;
+
 class Serie {
     private String $genre;
     private String $typePublic;
-    private Array $liste;
+    private array $liste;
     private String $titre;
     private String $descriptif; 
     private int $annee;
-    private String $dateAjout;
+    private DateTime $dateAjout;
     private int $nbEpisode;
     private String $cheminImage;
+    private $id;
 
-    public function __construct(String $genre, String $typePublic, Array $liste, String $titre, String $descriptif, int $annee, String $dateAjout, int $nbEpisode, String $cheminImage) {
+    public function __construct(String $genre, String $typePublic, String $titre, String $descriptif, int $annee, int $nbEpisode, String $cheminImage, $id = null) {
         $this->genre = $genre;
         $this->typePublic = $typePublic;
-        $this->liste = $liste;
+        // la liste d'épisode est vide par def
+        $this->liste = [];
         $this->titre = $titre;
         $this->descriptif = $descriptif;
         $this->annee = $annee;
-        $this->dateAjout = $dateAjout;
+        $this->dateAjout = new DateTime("now", new DateTimeZone('Europe/Paris'));
         $this->nbEpisode = $nbEpisode;
         $this->cheminImage = $cheminImage;
+        $this->id = $id;
     }
 
     public function __get($property) {
@@ -32,9 +39,18 @@ class Serie {
         throw new \Exception("Propriété inexistante: " . $property);
     }
 
-    public function addEpisode($episode) {
+    public function addEpisode(Episode $episode) {
+        if (!($episode instanceof Episode)) {
+            throw new \Exception("L'objet fourni n'est pas une instance de Episode.");
+        }
         $this->liste[] = $episode;
         $this->nbEpisode = count($this->liste);
     }
 
+    public function setId($id): void {
+        $this->id = $id;
+    }
+
+
+    // date("Y-m-d H:i:s");
 }
