055adf76822c82e72e2e734e3a35f853aae5347d
Florian HANNEZO <florian.hannezo5@etu.univ-lorraine.fr>
2025-11-04 11:16:03 +0100

    [FIX] corection namespace et ajout d'un getteur

--- src/classes/video/episode/Episode.php
+++ src/classes/video/episode/Episode.php
@@ -1,15 +1,24 @@
 <?php
 namespace iutnc\netvod\video\episode;
+
+
 class Episode {
     private int $numero;
     private string $titre;
-    private int $pduree;
+    private int $duree;
     private string $fileNameImage;
 
-    public function __construct(int $numero, string $titre, int $pduree, string $fileNameImage) {
+    public function __construct(int $numero, string $titre, int $duree, string $fileNameImage) {
         $this->numero = $numero;
         $this->titre = $titre;
-        $this->pduree = $pduree;
+        $this->duree = $duree;
         $this->fileNameImage = $fileNameImage;
+    }
+
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+        throw new \Exception("Propriété inexistante: " . $property);
     }
 }
