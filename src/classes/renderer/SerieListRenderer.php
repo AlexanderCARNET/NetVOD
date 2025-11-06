c8466701505ef29d684840d3f43ffb1dec22e73b
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2025-11-06 19:12:26 +0100

    [added] ajout de la méthode rendre du constructeur

--- src/classes/renderer/SerieListRenderer.php
+++ src/classes/renderer/SerieListRenderer.php
@@ -2,7 +2,31 @@
 
 namespace iutnc\netvod\renderer;
 
-class SerieListRenderer
-{
+use iutnc\netvod\video\lists\EnCours;
+use iutnc\netvod\video\lists\SerieList;
 
+class SerieListRenderer implements Renderer
+{
+    private SerieList $series;
+
+    public function __construct(SerieList $series){
+        $this->series=$series;
+    }
+    public function render(?int $t): string
+    {
+        $res="<table>";
+        if($this->series instanceof EnCours){
+            $res.="<thead><th>Titre</th><th>Episode en cours</th><th>Episode total</th></thead>";
+            foreach ($this->series->__get('series') as $serie){
+                $res.="<tr><td>{$serie->__get('titre')}</td><td>{$this->series->getEnCoursSerie($serie)}</td><td>{$serie->__get('nb_episodes')}</td></tr>";
+            }
+        }
+        else{
+            foreach ($this->series->__get('series') as $serie){
+                $renderSerie = new SerieRenderer($serie);
+                $res.="<tr>{$renderSerie->render(1)}</tr>";
+            }
+        }
+        return $res."</table>";
+    }
 }
